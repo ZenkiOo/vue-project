13cb3b48bb91bb94bfac45011a4b3cda6f380aed
ALEKSEI.KVASOW <aleksei.kvasow@gmail.com>
2021-01-18 16:30:01 +0300

    project init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <h1>Todo App</h1>
+    <ToDoList />
+  </div>
+</template>
+
+<script>
+import ToDoList from '@/components/ToDoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ToDoList
+  },
+};
+</script>
+
+<style lang="scss">
+body {
+  background-color: #333;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #999;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ToDoList.vue
@@ -0,0 +1,10 @@
+<template>
+   <div>
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+        </ul>   
+    </div> 
+</template>
